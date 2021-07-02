402b0982014b90c75ce1944ce53d26ad9db56931
SabineZilde <sabine.zilde@protonmail.ch>
2021-07-02 23:50:02 +0300

    Adding comm in output field

--- src/Components/Tags.js
+++ src/Components/Tags.js
@@ -1,16 +1,22 @@
 import { useState } from 'react';
 
 function Tags() {
-    const [inputName, setInputName] = useState('')
-    
+    const [inputName, setInputName] = useState('');
+    const [outputName, setOutputName] = useState('');
+
     const tagsCloud = ['Meeeeouw', 'cough', 'furball', 'into', 'food', 'bowl', 'then', 'scratch', 'owner', 'for', 'a', 'new', 'one', 'experiences', 'short', 'bursts', 'of', 'poo-phoria', 'after', 'going', 'to', 'the', 'loo', 'scratch', 'my', 'tummy', 'actually', 'i', 'hate', 'you', 'now', 'fight', 'me', 'so', 'freak', 'human', 'out', 'make', 'funny', 'noise', 'mow', 'mow', 'mow', 'mow', 'mow', 'mow', 'success', 'now', 'attack', 'human', 'yet', 'drink', 'from', 'the', 'toilet', 'but', 'sitting', 'in', 'a', 'box.', 'Show', 'belly.', 'Spit', 'up', 'on', 'light', 'gray', 'carpet', 'instead', 'of', 'adjacent', 'linoleum', 'good', 'now', 'the', 'other', 'hand,', 'too', 'check', 'cat', 'door', 'for', 'ambush', '10', 'times', 'before', 'coming', 'in'];
-    
+
     const tagsBadges = tagsCloud.map((tag) => {
         const arrayForMoreTags = inputName.split(' ');
         return arrayForMoreTags.indexOf(tag) !== -1 ? <span>&nbsp;<span className="badge bg-primary">{tag}</span></span> : <span>&nbsp;<span className="badge bg-secondary">{tag}</span></span>;
     })
 
-    const showInput = (e) => {
+    const inputArray = [];
+
+    const settingInput = (e) => {
+        inputArray.push(e.target.value);
+        const splittedInputArray = e.target.value.split(' ').join(', ');
+        setOutputName(splittedInputArray);
         return setInputName(e.target.value);
     }
 
@@ -18,12 +24,12 @@ function Tags() {
         <div>
             <div className="row">
                 <div className="col mt-3">
-                    <h2>Looking for tags: {inputName}</h2>
+                    <h2>Looking for tags: {outputName}</h2>
                 </div>
             </div>
             <div className="row">
                 <div className="col">
-                    <input onChange={showInput} />
+                    <input onChange={settingInput}/>
                 </div>
             </div>
             <div className="row">
